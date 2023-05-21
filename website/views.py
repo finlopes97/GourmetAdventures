66aef50b597f73caa06f3850d14bbb113197f688
keenanrm <keenan.martin16@gmail.com>
2023-05-22 00:02:58 +1000

    Templates ported into file layout

--- website/views.py
+++ website/views.py
@@ -1,8 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    return render_template('index.html')
+
+@bp.route('/user')
+def user():
+    return render_template('user.html')
+
+@bp.route('/event')
+def event():
+    return render_template('event.html')
+
+@bp.route('/create')
+def create():
+    return render_template('create.html')
+
+@bp.route('/bookings')
+def bookings():
+    return render_template('bookings.html')
