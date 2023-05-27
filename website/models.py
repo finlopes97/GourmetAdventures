b524db78f3df1cdad2957e3cf5221b10525213a5
keenanrm <keenan.martin16@gmail.com>
2023-05-27 16:12:02 +1000

    Added and tested event db

--- website/models.py
+++ website/models.py
@@ -1,9 +1,23 @@
 from flask_login import UserMixin
 from . import db
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    image = db.Column(db.String(400))
+    date = db.Column(db.DateTime, nullable=False)
+    startTime = db.Column(db.DateTime, nullable=False)
+    endTime = db.Column(db.DateTime, nullable=False)
+    price = db.Column(db.Numeric(precision=10, scale=2))
+
+    # Relationship to call event.comments and comment.event
+    comments = db.relationship('Comment', backref='event')
+	
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
