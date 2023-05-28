230eb18bb32ead6c695f7315bc905debda43aa05
Kyle <kyle.finlayson1997@gmail.com>
2023-05-28 11:43:42 +1000

    Added user table in models.py

--- website/models.py
+++ website/models.py
@@ -2,6 +2,21 @@ from flask_login import UserMixin
 from . import db
 from datetime import datetime
 
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.string(40), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    full_name = db.Column(db.String(120), nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    profile_image = db.Column(db.String(200), default='static/default.png', nullable=False)
+
+    comments = db.relationship('Comment', backref='user')
+
+    def __repr__(self):
+        return "<Username: {}>".format(self.username)
+
 class Event(db.Model):
     __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
