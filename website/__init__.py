1f3499b1b9ecccb125b0d4b4b13d28a3913eab3c
keenanrm <keenan.martin16@gmail.com>
2023-06-09 17:31:51 +1000

    Added error handling and pages for 404, 500

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,5 @@
 #from package import Class
-from flask import Flask 
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -7,11 +7,19 @@ from flask_wtf.csrf import CSRFProtect
 
 db=SQLAlchemy()
 
+def page_not_found(e):
+  return render_template('404.html'), 404
+
+def internal_server_error(e):
+  return render_template('500.html'), 500
+
 #create a function that creates a web application
 # a web server will run this web application
 def create_app():
   
     app=Flask(__name__)  # this is the name of the module/package that is calling this app
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, internal_server_error)
     app.debug=True
     app.secret_key='862b1a0d45e29873ab29da77a06728fed0d3c783512a797a'
     #set the app configuration data 
